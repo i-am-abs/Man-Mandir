c8baf8a9880b5adae546e379d00cb594c27b266b
i-am-abs <abs.createx@gmail.com>
2026-02-02 19:20:09 +0530

    added icon & god chalisa

---
+++ android/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.manmandir.app',
+  appName: 'Man Mandir',
+  webDir: 'www'
+};
+
+export default config;
